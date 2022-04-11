1a58fbcf5763746b9dcd3d56066986d055d51987
Matthew Adams <mattadams212003@gmail.com>
2022-04-10 21:00:50 -0400

    Added ability to register block to Registry class Added private function to register BlockItem when registering block Added BrassBlock class Added Brass Ore block including models and loot table

--- src/main/java/com/matthew/mattamsmod/brass/BrassModule.java
+++ src/main/java/com/matthew/mattamsmod/brass/BrassModule.java
@@ -1,10 +1,12 @@
 package com.matthew.mattamsmod.brass;
 
+import com.matthew.mattamsmod.brass.block.BrassBlocks;
 import com.matthew.mattamsmod.brass.item.BrassItems;
 import net.minecraft.item.Item;
 
 public class BrassModule{
     public static final BrassItems BRASS_ITEMS = new BrassItems();
+    public static final BrassBlocks BRASS_BLOCKS = new BrassBlocks();
 
     public BrassModule(){
 
---
+++ src/main/java/com/matthew/mattamsmod/brass/block/BrassBlocks.java
@@ -0,0 +1,17 @@
+package com.matthew.mattamsmod.brass.block;
+
+import com.matthew.mattamsmod.register.Registry;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+
+public class BrassBlocks {
+    public static final RegistryObject<Block> BRASS_ORE = Registry.registerBlock("brass_ore",
+            () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
+                    .harvestLevel(2)
+                    .harvestTool(ToolType.PICKAXE)
+                    .setRequiresTool()
+                    .hardnessAndResistance(5f)));
+}
